c7f1e80a104ee0b3ab3f2e8d2b8825b4f990933a
Kevin Ndung'u <kevgathuku@gmail.com>
2018-07-21 15:22:58 +0300

    Add mail actions menu
    Signed-off-by: Kevin Ndung'u <kevgathuku@gmail.com>

--- js/views/foldercontent.js
+++ js/views/foldercontent.js
@@ -159,8 +159,9 @@ define(function(require) {
 			var state;
 			switch (key) {
 				case 46:
+					// delete key
 					// Mimic a client clicking the delete button for the currently active message.
-					$('.mail-message-summary.active .icon-delete.action.delete').click();
+					$('.mail-message-summary.active').find('.icon-delete').click();
 					break;
 				case 39:
 				case 74:
